e211c811f72446deb2730c01014bbc2ca5f32976
matheusmarquesmota <matheusmarquesmota@hotmail.com>
2023-01-26 19:18:54 -0300

    Included a JS comment explaining the necessary prompts for the user.

--- script.js
+++ script.js
@@ -1,1 +1,24 @@
-document.write("hello world!")
+// PROMPT-BASED SO GENERATOR 
+
+/* 
+
+OBS: Due to some important details, this project will be written in portuguese.
+
+prompts needed, by order. 
+1. prazo.
+2. disponibilidade (cliente).
+3. observação opcional (ligar antes?).
+4. contato.
+5. problema.
+6. último sinal. 
+7. equipamentos no local. 
+8. detalhes:
+9. quedas de PPPoE (últimos 30 dias)
+10. IPv6 (status).
+11. visitas recentes (status).
+12. rua.
+13. bairro.
+14. cidade. 
+
+*/
+
